132538fe917999dbabd684252ca63ad98b8d9feb
minjeong073 <kim.mj80730@gmail.com>
2022-08-03 00:35:53 +0900

    [0802] spring
    - spring 연습문제 2 update

--- src/main/java/com/ming/spring/test/lifecycle/Test02Controller.java
+++ src/main/java/com/ming/spring/test/lifecycle/Test02Controller.java
@@ -5,8 +5,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ming.spring.test.lifecycle.model.Post;
 
 @RestController
 @RequestMapping("/lifecycle/test02")
@@ -60,5 +64,48 @@ public class Test02Controller {
 		
 		return list;
 	}
+	
+	@RequestMapping("/2")
+	public List<Post> jsonResponse2() {
+		
+		List<Post> list = new ArrayList<>();
+		
+		Post post;
+		post = new Post()
+		{{setTitle("안녕하세요 가입인사 드립니다");
+		setUser("hagulu");
+		setContent("안녕하세요 가입했어요. 앞으로 잘 부탁드립니다.");}};
+		
+		list.add(post);
+				
+		post = new Post()
+		{{setTitle("헐 대박");
+		setUser("bada");
+		setContent("오늘 목요일이었어... 금요일인줄");};};
+		
+		list.add(post);
+		
+		post = new Post()
+		{{setTitle("오늘 데이트 한 이야기 해드릴게요");
+		setUser("dulumary");
+		setContent("....");};};
+		
+		list.add(post);
+		
+		return list;
+	}
+	
+	@RequestMapping("/3")
+	public ResponseEntity<Post> responseEntity() {
+		Post post = new Post()
+		{{setTitle("안녕하세요 가입인사 드립니다");
+		setUser("hagulu");
+		setContent("안녕하세요 가입했어요. 앞으로 잘 부탁드립니다.");}};
+		
+		ResponseEntity<Post> entity = new ResponseEntity<>(post, HttpStatus.INTERNAL_SERVER_ERROR);
+		return entity;
+	}
+	
+	
 	
 }
