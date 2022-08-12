b8c72c69fe4988489c993a250a8f4892934af7a5
minjeong073 <kim.mj80730@gmail.com>
2022-08-12 18:57:38 +0900

    [0812] spring
    - jstl fmt 연습문제 3

--- src/main/java/com/ming/spring/test/jstl/JstlTestController.java
+++ src/main/java/com/ming/spring/test/jstl/JstlTestController.java
@@ -15,13 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class JstlTestController {
 
 	@GetMapping("/test01")
-	public String test01() {
+	public String jstlTest01() {
 		return "jstl/test01";
 	}
 	
 	
 	@GetMapping("/test02")
-	public String test02_1(Model model) {
+	public String jstlTest02(Model model) {
 		
 		// 1. JSTL forEach
 		
@@ -78,5 +78,46 @@ public class JstlTestController {
 		
 		
 		return "jstl/test02";
+	}
+	
+	
+	@GetMapping("/test03")
+	public String jstlTest03(Model model) {
+		
+		List<Integer> candidates = new ArrayList<>();
+		candidates.add(263001);
+		candidates.add(173942); 
+		candidates.add(563057); 
+		
+		model.addAttribute("candidates", candidates);
+		
+		
+		List<Map<String, Object>> cardBills = new ArrayList<>();
+
+		Map<String, Object> cardBill = new HashMap<>();
+		cardBill.put("store", "GS48");
+		cardBill.put("pay", 7800);
+		cardBill.put("date", "2025-09-14");
+		cardBill.put("installment", "일시불");
+		cardBills.add(cardBill);
+
+		cardBill = new HashMap<>();
+		cardBill.put("store", "현태백화점");
+		cardBill.put("pay", 2750000);
+		cardBill.put("date", "2025-09-18");
+		cardBill.put("installment", "3개월");
+		cardBills.add(cardBill);
+
+		cardBill = new HashMap<>();
+		cardBill.put("store", "요촌치킨");
+		cardBill.put("pay", 180000);
+		cardBill.put("date", "2025-09-20");
+		cardBill.put("installment", "일시불");
+		cardBills.add(cardBill);
+		
+		model.addAttribute("cardBillList", cardBills);
+		
+		return "jstl/test03";
+		
 	}
 }
